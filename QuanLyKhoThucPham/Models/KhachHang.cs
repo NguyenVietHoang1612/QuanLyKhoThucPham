d2d7fb5550b0a11410b10d5c928418584d116b12
TRANVAN TRANG <tranvantrang270603z@gmail.com>
2025-02-21 10:34:44 +0700

    Thêm model khachhang,nhacungcap

---
+++ QuanLyKhoThucPham/Models/KhachHang.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KhoThucPham.Models
+{
+    public class KhachHang
+    {
+        public KhachHang() { 
+            this.DSPhieuXuat = new HashSet<PhieuXuat>();
+        }
+
+        [Key]
+        public int MaKhachHang { get; set; }
+
+        public string HoTen {  get; set; }
+
+        public string DiaChi { get; set; }
+
+        public string Sdt { get; set; }
+
+        public virtual ICollection<PhieuXuat> DSPhieuXuat { get; set; }
+    }
+}
