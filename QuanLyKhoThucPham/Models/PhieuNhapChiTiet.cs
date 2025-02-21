7ab6b6b00e718d8ac6d58e25ca558d90a3c786d9
NguyenVietHoang1612 <hoang20312@gmail.com>
2025-02-21 10:33:46 +0700

    Them model phieunhap, phieuxuat

---
+++ QuanLyKhoThucPham/Models/PhieuNhapChiTiet.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KhoThucPham.Models
+{
+    public class PhieuNhapChiTiet
+    {
+        [Key]
+        public int MaPhieuNhapChiTiet { get; set; }
+
+        public int? MaSP { get; set; }
+
+        public int MaPhieuNhap {get; set;}
+
+        public int SoLuong {  get; set;}
+
+        public decimal DonGia { get; set;}  
+
+        public decimal TongTIen { get; set;}
+
+        public string GhiChu;
+
+        public PhieuNhap PhieuNhap { get; set; }
+        [ForeignKey("MaSP")]
+        public virtual SanPham SanPham { get; set; }
+    }
+}
---
+++ QuanLyKhoThucPham/Models/PhieuXuatChiTiet.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KhoThucPham.Models
+{
+    public class PhieuXuatChiTiet
+    {
+        [Key]
+        public int MaPhieuXuatChiTiet { get; set; }
+
+        public int? MaSP { get; set; }
+
+        public int MaPhieuXuat { get; set; }
+
+        public int SoLuong { get; set; }
+
+        public decimal DonGia { get; set; }
+
+        public decimal TongTIen { get; set; }
+
+        public string GhiChu;
+
+        public PhieuNhap PhieuXuat { get; set; }
+        [ForeignKey("MaSP")]
+        public virtual SanPham SanPham { get; set; }
+
+    }
+}
